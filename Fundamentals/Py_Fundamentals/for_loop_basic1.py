f192f6b81d00b178f88dd0fa329e0e600f57d41f
Jesse Greenough <jesse.greenough84@gmail.com>
2022-08-31 09:38:46 -0700

    practice utilizing loops

---
+++ Fundamentals/Py_Fundamentals/for_loop_basic1.py
@@ -0,0 +1,39 @@
+# x=0
+# while x <=150:
+#     print(x)
+#     x+=1
+
+
+# for x in range(0, 1001, 5):
+#     print(x)
+
+
+
+# for x in range (1, 100):
+#     if x%5==0:
+#         if x%10==0:
+#             print('Coding Dojo')
+#         else:
+#             print('Coding')
+#     else:
+#         print(x)
+
+
+# sum=0
+# y=0
+# while y<500000:
+#     if y%2==1:
+#         sum +=y
+#     y+=1
+# print(sum)
+
+
+# for x in range (2018, 0 ,-4):
+#     print(x)
+
+
+# lowNum=4
+# highNum=1997
+# mult=8
+# for Num in range(lowNum, highNum, mult):
+#     print(Num)
