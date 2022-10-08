231571942b05da4326bc0bc762d8f4da7f65e78a
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-08 05:05:44 -0700

    Added assignment Basketball Dictionaries

---
+++ Fundamentals/Py_Fundamentals/basketball_dictionaries.py
@@ -0,0 +1,80 @@
+# List of player dictionaries
+players = [
+    {
+    "name": "Kevin Durant", 
+    "age":34, 
+    "position": "small forward", 
+    "team": "Brooklyn Nets"
+    },
+    {
+    "name": "Jason Tatum", 
+    "age":24, 
+    "position": "small forward", 
+    "team": "Boston Celtics"
+    },
+    {
+    "name": "Kyrie Irving", 
+    "age":32,
+    "position": "Point Guard", 
+    "team": "Brooklyn Nets"
+    },
+    {
+    "name": "Damian Lillard", 
+    "age":33,
+    "position": "Point Guard", 
+    "team": "Portland Trailblazers"
+    },
+    {
+    "name": "Joel Embiid", 
+    "age":32,
+    "position": "Power Foward", 
+    "team": "Philidelphia 76ers"
+    },
+    {
+    "name": "DeMar DeRozan",
+    "age": 32,
+    "position": "Shooting Guard",
+    "team": "Chicago Bulls"
+    }
+]
+
+# Individual Player Dictionaries
+kevinData = {
+    "name": "Kevin Durant", 
+    "age":34, 
+    "position": "small forward", 
+    "team": "Brooklyn Nets"
+}
+jasonData = {
+    "name": "Jason Tatum", 
+    "age":24, 
+    "position": "small forward", 
+    "team": "Boston Celtics"
+}
+kyrieData = {
+    "name": "Kyrie Irving", 
+    "age":32,
+    "position": "Point Guard", 
+    "team": "Brooklyn Nets"
+}
+
+# Player class updated to take in full dictionaries
+class Player:
+    def __init__(self, playerData):
+        self.name = playerData['name']
+        self.age = playerData['age']
+        self.position = playerData['position']
+        self.team = playerData['team']
+
+    # Create a class function that creates team of Player objects using a list of player data dictionaries
+    def get_team(team_list):
+        new_team = []
+        for player in players:
+            new_player = Player(player)
+            new_team.append(new_player)
+        return new_team
+
+# Creating player objects using dictionaries of player data
+kevin = Player(kevinData)
+jason = Player(jasonData)
+kyrie = Player(kyrieData)
