5596790ebe70fac7b334598fe552bff46a2976af
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-08 06:10:06 -0700

    Created pet class with methods as module

---
+++ Fundamentals/Py_Fundamentals/dojo_pets/Pet.py
@@ -0,0 +1,21 @@
+
+class Pet:
+    def __init__(self, name, type, tricks, health, energy):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+        self.health = health
+        self.energy = energy
+
+    def sleep(this):
+        this.energy += 25
+
+    def eat(this):
+        this.energy += 5
+        this.health += 10
+
+    def play(this):
+        this.health += 5
+
+    def noise(this):
+        print('Bark Bark')
