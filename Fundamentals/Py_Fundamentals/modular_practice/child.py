61b6009b53776dafa64e6b7211885358c5aec433
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-08 05:36:02 -0700

    Created clas to import parent as module

---
+++ Fundamentals/Py_Fundamentals/modular_practice/child.py
@@ -0,0 +1,6 @@
+# Import the parent file (as a module) and see the difference in print statements.
+import parent
+
+# Check all the variables provided from the module import
+# Namely look for the '__name__' variable
+print(locals())
